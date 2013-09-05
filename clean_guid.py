e7f899bea3a66d9834ca5beb7ea5fe6a4aec80f5
Piotr Mitros <pmitros@mitx.mit.edu>
2013-09-05 17:16:18 -0400

    Code fixed to not run out of names/courses

--- clean_guid.py
+++ clean_guid.py
@@ -18,15 +18,15 @@ else:
 
 def generate_course_name():
     universities = ["MIT", "Harvard", "Berkeley", "Stanford", "IIT", "Udacity", "Kaplan"]
-    prefixes = ["Introductory", "Advanced", "Seminar on", "Quantum"]
-    courses = ["Chemistry", "Physics", "Biology", "Informatics", "Computation", "Poetry", "Philosophy", "Agriculture"]
-    suffixes = ["", "for Dummies", "for Inner City Youth", "On-Line Edition", "with Applications", "with Honors", "for the Determined"]
+    prefixes = ["Introductory", "Advanced", "Seminar on", "Quantum", "Computational", "Freshman", "Australian", "Ethics of"]
+    courses = ["Chemistry", "Physics", "Biology", "Informatics", "Computation", "Poetry", "Philosophy", "Agriculture", "Anatomy", "Electrophysiology", "Hackery", "Learning Sciences", "Mechanical Engineering", "UX Design", "Architecture", "Computer Architecture", "History", "Marketing", "Business Law", "Military Strategy", "Foreign Relations"]
+    suffixes = ["", "for Dummies", "for Inner City Youth", "On-Line Edition", "with Applications", "with Honors", "for the Determined", "Night School"]
 
     n = list(itertools.product(universities, prefixes, courses, suffixes))
     random.shuffle(n)
     
     for item in n:
-        yield " ".join(item)
+        yield " ".join(item)+" "+str(random.randint(1,9)*100+random.randint(0,1)*10+random.randint(0,5))
 
 
 student_generator = iter(student_names)
@@ -40,6 +40,11 @@ with open("../SakaiEvents.csv", "rb") as csvfile:
     first_row = reader.next()
     print "{},{},{},{}".format(*first_row)
     for row in reader:
-        user = user_names.setdefault(row[0], student_generator.next())
-        course = course_names.setdefault(row[3], course_generator.next())
+        gu = row[0]
+        if row[0] not in user_names: 
+            user_names[gu] = student_generator.next()
+        user = user_names[gu]
+        if row[3] not in course_names: 
+            course_names[row[3]] = course_generator.next()
+        course = course_names[row[3]]
         print "{},{},{},{}".format(user, row[1], row[2], course)
