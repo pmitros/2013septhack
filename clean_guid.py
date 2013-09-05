f35379cef36cc0800daccdb2108f7c3b074058a8
Piotr Mitros <pmitros@mitx.mit.edu>
2013-09-05 16:04:25 -0400

    Changes

--- clean_guid.py
+++ clean_guid.py
@@ -1,19 +1,29 @@
 # ~6000 users
 # ~800 courses
 
-import csv, itertools, random
+import csv, itertools, random, names, os.path
 
-last_names = open("names/names/dist.all.last").readlines()
-first_names = open("names/names/dist.male.first").readlines()+open("names/names/dist.female.first").readlines()
-names = itertools.product(first_names, last_names)
-names = random.shuffle(names)
-print len(names)
+if os.path.exists("names.txt"):
+    n = open("names.txt").readlines()
+else: 
+    n = set()
+    while len(n) < 6000:
+        n.add(names.get_full_name())
+    n = list(n)
+    random.shuffle(n)
+    f = open("names.txt","w")
+    f.writelines(n)
+    f.close()
 
-users = {}
-courses = {}
+universities = ["MIT", "Harvard", "Berkeley", "Stanford", "IIT", "Udacity", "Kaplan"]
+prefixes = ["Introductory", "Advanced", "Seminar on", "Quantum"]
+courses = ["Chemistry", "Physics", "Biology", "Informatics", "Computation", "Poetry", "Philosophy", "Agriculture"]
+suffixes = ["", "for Dummies", "for Inner City Youth", "On-Line Edition", "with Applications", "with Honors"]
 
+n = itertools.product()
+random.shuffle(n)
 
-with open('SakaiEvents.csv', 'rb') as csvfile:
+with open('../SakaiEvents.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     for row in reader:
         pass
