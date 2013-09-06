a2f3cbb646f8ab776c824d4f17aa4fdff5c6942c
James Tauber <jtauber@jtauber.com>
2013-09-06 13:24:20 -0400

    style tweaks

--- send_events.py
+++ send_events.py
@@ -7,18 +7,20 @@ from logging.handlers import HTTPHandler
 with open("../cleaned.csv", "rb") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     first_row = reader.next()
-    logger=logging.getLogger('sendevent')
+    logger=logging.getLogger("sendevent")
     logger.addHandler(HTTPHandler(sys.argv[1], sys.argv[2]))
 
-    i=0
+    i = 0
 
     for row in reader:
-        event = {'username':row[0], 
-                 "time":row[1],
-                 '''event_type''':"sakai."+row[2], 
-                 """course""":row[3], 
-                 'event':{}, 
-                 "event_source": "playback"}
+        event = {
+            "username": row[0],
+             "time": row[1],
+             "event_type": "sakai." + row[2],
+             "course": row[3],
+             "event": {},
+             "event_source": "playback"
+        }
     
         logger.error(json.dumps(event))
         
