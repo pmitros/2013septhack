29f0598104b94c36d5cbfe02618a50b36ac80451
Piotr Mitros <pmitros@mitx.mit.edu>
2013-09-06 10:12:35 -0400

    Activity histograms appear to work.

--- sakaianalytic/activitymodule/percourse/activity.py
+++ sakaianalytic/activitymodule/percourse/activity.py
@@ -9,3 +9,17 @@ def dump_to_db(mongodb, events):
     collection = mongodb['event_log']
     collection.insert([e.event for e in events])
 
+@event_handler()
+def histogram(mongodb, events):
+    collection = mongodb['course_activity']
+    for evt in events: 
+        event_type = evt['event_type']
+        if event_type.split('.')[0] == 'sakai': 
+            course = evt['course']
+            collection.update({'course':course}, 
+                              {'$inc':{'count':1}}, 
+                              True, 
+                              False)
+            print "Got a message", evt
+            
+    pass
--- send_events.py
+++ send_events.py
@@ -10,12 +10,18 @@ with open("../cleaned.csv", "rb") as csvfile:
     logger=logging.getLogger('sendevent')
     logger.addHandler(HTTPHandler(sys.argv[1], sys.argv[2]))
 
+    i=0
+
     for row in reader:
         event = {'username':row[0], 
                  "time":row[1],
-                 '''event_type''':row[2], 
+                 '''event_type''':"sakai."+row[2], 
                  """course""":row[3], 
                  'event':{}, 
                  "event_source": "playback"}
     
         logger.error(json.dumps(event))
+        
+        i += 1
+        if i % 100 == 0:
+            print i
